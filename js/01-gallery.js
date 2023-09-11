69d61b050c0f7c0495f5380c7332f3f2e4ecea0e
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-09-11 21:33:14 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+ <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+ </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", handleClickGallery);
+
+function handleClickGallery(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" alt="${evt.target.alt}" />`,
+    {
+      onClose: () => {
+        document.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
+  instance.show();
+
+  document.addEventListener("keydown", closeModal);
+  function closeModal(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+    console.log(evt.code);
+  }
+}
